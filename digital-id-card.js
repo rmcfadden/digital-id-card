cae5b6ea872f70444a013cb2484770ae315e922a
Ryan McFadden <ryanpmfadden@gmail.com>
2015-11-21 15:17:44 -0800

    Start card ui.

--- digital-id-card.js
+++ digital-id-card.js
@@ -1,16 +1,81 @@
 (function($){
-   $.fn.digitalIdCard = function(options){
+  $.fn.digitalIdCard = function(methodOrOptions){
+  	if ( methods[methodOrOptions] ) {
+			return methods[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof methodOrOptions === 'object' || ! methodOrOptions ) {
+			// Default to "init"
+			return methods.init.apply( this, arguments );
+		} else {
+			$.error( "Method " +  methodOrOptions + " does not exist on jQuery.digitalIdCard" );
+  	}
+
+  }
+
+  var methods = {
+  	init : function(options) {
+
       var defaults = {
-        height: 500px,
-        width: 5000px,
-      }
+        height : "640px",
+        width : "360px"
+      };
 
-      var settings = $.extend({}, defaults, options);
+      var settings = $.extend({}, defaults, options); 			
 
-      if(settings.variable1){
-         // do something
-      }
+     	var Content = _createContent();	
 
-      ...
-   }
+      $(this).html(Content);
+
+    	return this;
+		},
+
+		show : function( ) {},
+		hide : function( ) {},
+		onUpdate : function() {}
+	}
+
+	function _toJson()
+	{
+
+	}
+
+	function _createContent()
+	{
+      var Content = '<div id="id-card-container">'
+
+      // Header
+      Content += '<div id="id-card-header">'
+
+     	Content += '</div>';	// id-card-header
+
+      // Content
+      Content += '<div id="id-card-content">'
+
+      // Image
+      Content += '<div id="id-card-image">'
+
+     	Content += '</div>';	// id-card-image
+
+      // Description
+      Content += '<div id="id-card-desciption">'
+
+     	Content += '</div>';	// id-card-description
+
+
+     	Content += '</div>';	// id-card-content
+
+      // Footer
+      Content += '<div id="id-card-footer">'
+
+     	Content += '</div>';	// id-card-footer
+
+     	Content += '</div>';	// id-card-container
+
+  		var Content = $(Content);
+
+
+  		return Content;
+	}
+
+
+
 })(jQuery);
